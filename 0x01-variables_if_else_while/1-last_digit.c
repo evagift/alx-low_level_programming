492dc76b3c00499cab6466ea86e642bd4aa8727f
evagift <talkeva1@gmail.com>
2023-02-17 15:27:39 -0800

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,17 +1,32 @@
+#include <stdlib.h>
+#include <time.h>
 #include <stdio.h>
+
 /**
  * main - Entry point
- * Description: 'A program that prints alph in l.case'
- * Return: Always 0 (success)
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char c;
+	int n;
+	int last_digit;
 
-	for (c = 'a'; c <= 'z'; ++c)
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	last_digit = n % 10;
+	printf("Last digit of %d is %d ", n, last_digit);
+	if (last_digit > 5)
 	{
-	putchar(c);
+	printf("and is greater than 5\n");
 	}
-	putchar('\n');
+	else if (last_digit == 0)
+	{
+	printf("and is 0\n");
+	}
+	else
+	{
+	printf("and is less than 6 and not 0\n");
+	}
 	return (0);
 }
