03a10b4d9c0e05e9f9a099a3a63d060be8b30651
evagift <talkeva1@gmail.com>
2023-04-09 07:41:38 -0700

    0x14-bit_manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,26 +7,29 @@
  * Return: the converted number, or 0 if there is one or more chars in the string
  *         b that is not 0 or 1 or if b is NULL
  */
-unsigned int binary_to_uint(const char *b) {
-    unsigned int num = 0;
-    int i = 0;
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int total = 0, power = 1;
+	int len = 0;
 
-    if (b == NULL) {
-        return 0;
-    }
+	if (b == NULL)
+		return (0);
 
-    while (b[i] != '\0') {
-        if (b[i] == '0' || b[i] == '1') {
-            num = num << 1;  // shift num left by 1 bit
-            if (b[i] == '1') {
-                num = num | 1;  // set least significant bit of num to 1
-            }
-            i++;
-        } else {
-            return 0;
-        }
-    }
+	/* determine length of the string */
+	while (b[len] != '\0')
+	{
+		if (b[len] != '0' && b[len] != '1')
+			return (0);
+		len++;
+	}
 
-    return num;
+	/* convert binary to decimal */
+	for (len--; len >= 0; len--)
+	{
+		if (b[len] == '1')
+			total += power;
+		power *= 2;
+	}
+
+	return (total);
 }
-
