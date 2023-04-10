1de5abf5653a01915cd5750fd034990729f388b4
evagift <talkeva1@gmail.com>
2023-04-10 12:31:42 -0700

    0x14-bit_manipulation

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
  * get_endianness - check endianness
+ *
  * Return: 0 if big endian, or 1 if little endian
  */
 int get_endianness(void)
@@ -12,5 +13,5 @@ int get_endianness(void)
 	}
 	u;
 	u.i = 1;
-	return u.c[0] == 1 ? 1 : 0;
+	return ((u.c[0] == 1) ? 1 : 0);
 }
